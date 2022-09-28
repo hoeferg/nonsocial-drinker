f5374d6eaa00cebf8e37996269c5920d5fb61f8e
nixon <jeromenixon95@gmail.com>
2022-09-28 13:19:29 -0700

    set about us into p tags and app info into p tags

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,35 @@
-//  Create Summary Section
-// //const $aboutUS = document.querySelector('#about-us');
-// const $thisApp =document.querySelector('#this-app');
-
-function hideAboutUS() {
-  $aboutUS.setAttribute("class", "custom-display ");
-}
 
 function createAboutUs() {
-  const $aboutUS = document.createElement("div");
-  const $AboutUsDesc = document.createElement("p");
-  const $appDiv = document.createElement("div");
-  const $appDesc = document.createElement("p");
+  const $aboutUsDiv = document.createElement('div');
+  const $aboutUsDesc1 = document.createElement('p');
+  const $aboutUsDesc2 = document.createElement('p');
+  const $aboutUsDesc3 = document.createElement('p');
 
-  document.body.appendChild($aboutUS);
-  $aboutUS.appendChild($AboutUsDesc);
-  document.body.appendChild($appDiv0);
-  $appDesc.appendChild($appDesc);
+  document.body.appendChild($aboutUsDiv);
+  $aboutUsDiv.appendChild($aboutUsDesc1);
+  $aboutUsDiv.appendChild($aboutUsDesc2);
+  $aboutUsDiv.appendChild($aboutUsDesc3);
 
-  $AboutUsDesc.textContent =
-    "It is our hope that you will find this app fun while you are out in social situations.";
-  ("We only say that the app is for the socially awkward because it was built by the socially awkward.");
-  ("It’s like that weird item on your friends coffee table that prompts conversation and inspires connection.");
+  $aboutUsDesc1.textContent =
+    'It is our hope that you will find this app fun while you are out in social situations.';
+    $aboutUsDesc2.textContent =
+  'We only say that the app is for the socially awkward because it was built by the socially awkward.';
+  $aboutUsDesc3.textContent = 
+  ' Its like that weird item on your friends coffee table that prompts conversation and inspires connection.';
+  console.log($aboutUsDesc1.textContent);
 
+  const $appDiv = document.createElement('div');
+  const $appDesc = document.createElement('p');
+  const $appDesc2 = document.createElement('p');
+  document.body.appendChild($appDiv);
+  $appDiv.appendChild($appDesc);
+  $appDiv.appendChild($appDesc2);
   $appDesc.textContent =
-    "This app will generate a cocktail, a joke and an excuse to get yourself on those getaway sticks and hightail it out of any truly awkward situation.";
-    Not only is it an app for the socially awkward but it can help you escape the socially awkward… PURE, GENIUS!
+    'This app will generate a cocktail, a joke and an excuse to get yourself on those getaway sticks and hightail it out of any truly awkward situation.';
+    $appDesc2.textContent =
+  'Not only is it an app for the socially awkward but it can help you escape the socially awkward… PURE, GENIUS!';
+
+  console.log($appDesc);
 }
+ createAboutUs();
+
