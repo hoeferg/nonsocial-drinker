e3a11dd0a1e2b2dcde84632599e0b44fc471b7fd
hoeferg <111393796+hoeferg@users.noreply.github.com>
2022-09-28 11:01:22 -0700

    joke api is working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,27 @@
-const $header  = document.querySelector('#greeting');
+const $header = document.querySelector('#greeting');
 const test = document.querySelector('#test');
 
 
 
 // This function will hide the header when called
 function hideHeader() {
-    $header.setAttribute("class","custom-display");
+    $header.setAttribute("class", "custom-display");
 }
 
 // This function can be passed a parent element and will remove its children
 function refreshDisplay(parentElement) {
 
-    while(parentElement.firstChild) {
+    while (parentElement.firstChild) {
 
         parentElement.removeChild(parentElement.firstChild);
 
     }
 
 }
-
 // hideHeader();
+
 function getJoke() {
-    // JokeAPI.getJokes()
-    const requestUrl =`https://sv443.net/jokeapi/v2`
+    const requestUrl = `https://sv443.net/jokeapi/v2`
     console.log(jokeInput)
     fetch(requestUrl)
         .then((res) => res.json())
