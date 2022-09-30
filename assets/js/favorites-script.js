3421650f5b08613d95e821566948b0fa0d139c86
T <endlesstruffle80@gmail.com>
2022-09-30 15:30:16 -0700

    save work

---
+++ assets/js/favorites-script.js
@@ -0,0 +1,4 @@
+const $favoriteDrinksList = document.querySelector('#favorite-drinks-list')
+const $favoriteJokesList = document.querySelector('#favorite-jokes-list')
+const $favoriteExcusesList = document.querySelector('#favorite-excuses-list')
+const $clearAll = document.querySelector('#clear-all')
