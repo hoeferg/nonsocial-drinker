da7db0ab3ed64b85ea974f2850933c859c022af4
hoeferg <111393796+hoeferg@users.noreply.github.com>
2022-09-30 20:46:29 -0700

    added clear all button

--- assets/js/favorites-script.js
+++ assets/js/favorites-script.js
@@ -1,9 +1,10 @@
-const $favoriteDrinksList = document.querySelector('#favorite-drinks-list')
-const $favoriteJokesList = document.querySelector('#favorite-jokes-list')
-const $favoriteExcusesList = document.querySelector('#favorite-excuses-list')
-const $clearAll = document.querySelector('#clear-all')
+const $favoriteDrinksList = document.querySelector('#last-drink-list')
+const $favoriteJokesList = document.querySelector('#last-jokes-list')
+const $favoriteExcusesList = document.querySelector('#last-excuses-list')
+const $clearAllBtn = document.querySelector('#clear-all')
 
-$favoriteDrinksList.addEventListener('click', function(event) {
+
+$favoriteDrinksList.addEventListener('click', function (event) {
     let btn = event.target.getAttribute('data-name')
     let emptyArray = []
     if (localStorage.getItem('drinkList') !== null) (
@@ -13,9 +14,9 @@ $favoriteDrinksList.addEventListener('click', function(event) {
         emptyArray.push(btn)
         localStorage.setItem('drinkList', JSON.stringify(emptyArray))
     }
-} );
+})
 
-$favoriteJokesList.addEventListener('click', function(event) {
+$favoriteJokesList.addEventListener('click', function (event) {
     let btn = event.target.getAttribute('data-name')
     let emptyArray = []
     if (localStorage.getItem('jokeList') !== null) (
@@ -25,9 +26,9 @@ $favoriteJokesList.addEventListener('click', function(event) {
         emptyArray.push(btn)
         localStorage.setItem('jokeList', JSON.stringify(emptyArray))
     }
-} );
+});
 
-$favoriteExcusesList.addEventListener('click', function(event) {
+$favoriteExcusesList.addEventListener('click', function (event) {
     let btn = event.target.getAttribute('data-name')
     let emptyArray = []
     if (localStorage.getItem('excuseList') !== null) (
@@ -37,4 +38,28 @@ $favoriteExcusesList.addEventListener('click', function(event) {
         emptyArray.push(btn)
         localStorage.setItem('excuseList', JSON.stringify(emptyArray))
     }
-} );
+});
+
+function refreshDisplay(parentElement) {
+
+    while (parentElement.firstChild) {
+
+        parentElement.removeChild(parentElement.firstChild);
+
+    }
+
+}
+
+function clearAll() {
+    localStorage.removeItem('savedDrinks')
+    localStorage.removeItem('savedJokes')
+    localStorage.removeItem('savedExcuses')
+    localStorage.removeItem('gotoDrinkList')
+    localStorage.removeItem('gotoJokeList')
+    localStorage.removeItem('gotoExcuseList')
+    refreshDisplay ($favoriteDrinksList)
+    refreshDisplay ($favoriteJokesList)
+    refreshDisplay ($favoriteExcusesList)
+}
+
+$clearAllBtn.addEventListener('click', clearAll)
